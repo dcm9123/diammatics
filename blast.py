b12f3d2ebbf93076f0ab37fe9b18ce0f56003ba2
dcm9123 <35580294+dcm9123@users.noreply.github.com>
2024-06-08 16:13:15 -0600

    Update blast.py

--- blast.py
+++ blast.py
@@ -6,6 +6,10 @@ parser = argparse.ArgumentParser(description="This program was created with the 
 parser.add_argument("--database", type=str, help="The name of the database that is taken from the NCBI website")
 parser.add_argument("--file", type=str, help="This is the name and path of your multifasta file")
 
+args = parser.parse_args()
+database = args.database
+fasta_file = args.file
+
 def blast_fasta(fasta_file, database):
     sequences = list(SeqIO.parse(fasta_file,"fasta"))
     for seq in sequences:
@@ -18,8 +22,4 @@ def blast_fasta(fasta_file, database):
 
         print(f"The results for {seq.id} saved to {output_file}")
 
-fasta_file = "/bulk/IMCshared_bulk/sycuro_shared_projects/Twist96/novaseq_run/analysis/plate2/contigs_1000/all_output/16S/all_16S.fasta"
-database = "nt"
-
 blast_fasta(fasta_file,database)
-
