787f57e980e56c776a96f32147b9328ab74d98e9
Daniel Castaneda Mogollon, PhD <35580294+dcm9123@users.noreply.github.com>
2025-07-01 00:32:06 -0600

    Update raxml_slurm.sh

--- T1D/raxml_slurm.sh
+++ T1D/raxml_slurm.sh
@@ -13,10 +13,6 @@ raxml-ng --bootstrap --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration
 
 raxml-ng --bootstrap --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_S5_species_aligned.fasta --prefix S5 --seed 100 --threads 2 --model GTR+G
 
-raxml-ng --bootstrap --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_NS6_species_aligned.fasta --prefix NS6 --seed 100 --threads 2 --model GTR+G
+raxml-ng --search --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_S2_species_aligned.fasta --model GTR+G --prefix S2_ML --seed 100 --threads 4
 
-raxml-ng --search --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_S2_species_aligned.fasta --model GTR+G --prefix S2_ML --seed 100 threads 1 --force
-
-raxml-ng --search --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_S5_species_aligned.fasta --model GTR+G --prefix S5_ML --seed 100 threads 1 --force
-
-raxml-ng --search --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_NS6_species_aligned.fasta --model GTR+G --prefix NS6_ML --seed 100 threads 1 --force
+raxml-ng --search --msa /bulk/sycuro_bulk/daniel/diabetes/UC_UT_collaboration/16S_data/16S_after_fixing_and_discussion/good_files_to_use/MAFFT_consortia/rm_dupt1d_db2_withDups_S_S5_species_aligned.fasta --model GTR+G --prefix S5_ML --seed 100 --threads 4
